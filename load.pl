bd28256f6e7cb5855e9df5c6791ee1141252834b
Wouter Beek <me@wouterbeek.com>
2013-11-09 10:46:21 +0100

    Rewrote project initialization.

--- load.pl
+++ load.pl
@@ -1,33 +1,10 @@
 % The load file for the EnergyLabels project.
 
-project_name('EnergyLabels').
+:- multifile(user:project_name/1).
+user:project_name('EnergyLabels').
 
-:- initialization(load_energylabels).
-
-load_energylabels:-
-  source_file(load_energylabels, ThisFile),
-  file_directory_name(ThisFile, ThisDirectory),
-  % By asserting the STCN directory as mnemonic =project= we can refer
-  % to this from within the PGC (which does not 'know' that STCN is
-  % using it).
-  assert(user:file_search_path(project, ThisDirectory)),
-  assert(user:file_search_path(el, ThisDirectory)),
+load_energylabels(Dir):-
+  assert(user:file_search_path(el, Dir),
   assert(user:file_search_path(data, el('Data'))),
-  assert(user:file_search_path(output, data('Output'))),
-  load_pgc(el).
-
-load_pgc(_Project):-
-  user:file_search_path(pgc, _Spec), !.
-load_pgc(Project):-
-  Spec =.. [Project,'PGC'],
-  assert(user:file_search_path(pgc, Spec)),
-  load_or_debug(pgc).
-
-load_or_debug(Project):-
-  predicate_property(debug_project, visible), !
-  Spec =.. [Project,debug],
-  ensure_loaded(Spec).
-load_or_debug(Project):-
-  Spec =.. [Project,load],
-  ensure_loaded(Spec).
+  assert(user:file_search_path(output, data('Output'))).
 
