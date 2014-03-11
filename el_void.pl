8bc843bcd17b52af3b340d48e2f07d246a190a7d
Wouter Beek <me@wouterbeek.com>
2014-03-11 16:19:06 +0100

    Sync PGC

--- el_void.pl
+++ el_void.pl
@@ -24,7 +24,7 @@ Asserts the VoID description of the energy labels dataset.
 :- use_module(void(void_db)). % XML namespace.
 :- use_module(void(void_stat)).
 :- use_module(xml(xml_namespace)).
-:- use_module(xsd(xsd_dateTime)).
+:- use_module(xsd(xsd_dateTime_ext)).
 
 :- xml_register_namespace(dbpedia, 'http://dbpedia.org/resource/').
 :- xml_register_namespace(dcterms, 'http://purl.org/dc/terms/').
